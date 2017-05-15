71cb6e2a9e8437bb52870c70840c2c55e5c79359
Geraldo Megale <megale.petrobras.com.br>
2017-05-15 09:32:25 -0300

    creating gulp build tasks

--- gulp/build.js
+++ gulp/build.js
@@ -1,27 +1,9 @@
 
 (function(){
 
+
     var gulp = require('gulp');
-    var concat = require('gulp-concat');  
-    var rename = require('gulp-rename');  
-    var uglify = require('gulp-uglify'); 
 
-    var jsFiles = ['src/app/config/**/*.js', 'src/app/component/**/*.js', 'src/app/controller/**/*.js'];
-    var jsDest = 'dist';
-
-    gulp.task('build', function() {
-
-        return gulp.src(jsFiles)
-
-            .pipe(concat('gmBackToTop.js'))
-
-            .pipe(gulp.dest(jsDest))
-
-            .pipe(rename('gmBackToTop.min.js'))
-
-            .pipe(uglify())
-
-            .pipe(gulp.dest(jsDest));
-    });
+    gulp.task('build', ['buildJS', 'buildCSS']);
 
 }());
