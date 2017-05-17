e313c5be8d27351ccf36806021aba3d26429217e
Geraldo Megale <megale.petrobras.com.br>
2017-05-17 16:55:25 -0300

    ph test

--- karma.conf.js
+++ karma.conf.js
@@ -69,16 +69,6 @@ module.exports = function(config) {
     // enable / disable watching file and executing tests whenever any file changes
     autoWatch: true,
 
-
-    // start these browsers
-    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['phantomJS'],
-
-
-    // Continuous Integration mode
-    // if true, Karma captures browsers, runs the tests and exits
-    singleRun: false,
-
     // Concurrency level
     // how many browser should be started simultaneous
     concurrency: Infinity
