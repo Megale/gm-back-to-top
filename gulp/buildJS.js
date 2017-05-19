fb9a5f61203ac7797b8819261f0b2b546556e824
Geraldo Megale <megale.petrobras.com.br>
2017-05-19 10:46:00 -0300

    cleaning gh-pages

---
+++ gulp/buildJS.js
@@ -0,0 +1,25 @@
+var gulp = require('gulp');
+var concat = require('gulp-concat');  
+var rename = require('gulp-rename');  
+var uglify = require('gulp-uglify'); 
+var minify = require('gulp-minify');
+var ngAnnotate = require('gulp-ng-annotate')
+
+(function(){
+
+    var jsFiles = ['src/app/config/**/*.js', 'src/app/component/**/*.js', 'src/app/controller/**/*.js'];
+    var jsDest = 'dist';
+
+    gulp.task('buildJS', function() {
+
+        return gulp.src(jsFiles)
+
+            .pipe(ngAnnotate())
+
+            .pipe(concat('gmBackToTop-min.js'))
+
+            .pipe(uglify())
+
+            .pipe(gulp.dest(jsDest));
+    });
+}());
---
+++ src/app/config/backToTop.run.js
@@ -0,0 +1,13 @@
+(function(){
+  'use strict';
+
+  angular
+    .module('backToTop')
+    .run(runBlock);
+
+  /** @ngInject */
+  function runBlock($log) {
+
+    $log.debug('backToTop module loaded');
+  }
+})();
