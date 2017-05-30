b7db9f86ce63445eb7e5a44037ae6e25dcb34dd0
Geraldo Megale <megale.petrobras.com.br>
2017-05-30 09:23:48 -0300

    runblock inserted

---
+++ src/app.run.js
@@ -0,0 +1,18 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('app')
+    .run(runBlock);
+
+  /** @ngInject */
+  function runBlock($log) {
+
+    $log.debug('app module loaded');
+  }
+
+
+
+
+
+})();
